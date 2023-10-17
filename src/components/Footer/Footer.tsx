19b04482d7860be754bc620ed927da4d65cdba96
João Paulo Andrade Raimundo <62303009+jpegame@users.noreply.github.com>
2023-10-17 17:03:22 -0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,10 @@ export default function Footer(){
     return(
         <>
            <footer>
-            .
+            
+            <div className='NextPage'></div>
+            <div className='Page'><h1>1/10</h1></div>
+            <div className='BackPage'></div>
            </footer>
         </>
     )
