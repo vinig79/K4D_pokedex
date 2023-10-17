9007dd531f0b2d85f5ec2575bb4004dfb971fd6e
vinig79 <viniciusgroto@gmail.com>
2023-10-17 11:41:20 -0300

    align itens

--- src/components/PokemonTile/PokemonTile.tsx
+++ src/components/PokemonTile/PokemonTile.tsx
@@ -1,12 +1,15 @@
 import './PokemonTile.scss';
 import pokBoll from '../../assets/pokeball-watermark.png';
 
-export default function PokemonTile() {
+export default function PokemonTile(){
     return (
         <>
             <div className='PokemonTile'>
                 <img src={pokBoll} alt="" />
-                <div className='id'><p>#id</p></div>
+                <div className='data'>
+                    <p id='id'>#id</p>
+                    <p id='nome'>Pikachu</p>
+                </div>
             </div>
         </>
     )
